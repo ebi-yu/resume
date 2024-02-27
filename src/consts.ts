d50b63636f7eebfc6397662ba8c2a647ad1b84b9
ebi-yu <ebi-yu@lakeel.com>
2024-02-27 13:03:12 +0900

    🚧 全体の構成を変更

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,9 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = "Ebi-yu Site";
+export const SITE_DESCRIPTION = "Welcome to my website!";
+export const TWITTER_URL = "https://twitter.com/Nicasdream0";
+export const GIT_HUB_URL = "https://github.com/ebi-yu";
+export const QITA_URL = "https://qiita.com/Nicasdream0";
+export const ZEEN_URL = "https://zenn.dev/ebi_yu";
